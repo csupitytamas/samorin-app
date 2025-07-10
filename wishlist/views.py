a3518d382457fbc6cb55ac4739a829dfd8e324aa
CsupityTamás <160401832+csupitytamas@users.noreply.github.com>
2025-07-10 08:28:44 +0200

    Feat: USERs and roles, profile page.

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,10 +1,16 @@
 from rest_framework import viewsets
 from .models import Wishlist
+from .permissions import IsChiefOrAdmin
 from .serializers import WishlistSerializer
+
 
 class WishlistViewSet(viewsets.ModelViewSet):
     queryset = Wishlist.objects.all()
+    permission_classes = [IsChiefOrAdmin] # vagy a saját IsChiefOrAdmin, ha csak ők használhatják
     serializer_class = WishlistSerializer
+
+    def get_queryset(self):
+        return Wishlist.objects.filter(created_by=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
